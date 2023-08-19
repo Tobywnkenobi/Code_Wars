50e6cc9c0335332973046f861356ccd92c230278
TobyM <tobymarkham@gmail.com>
2023-08-19 10:17:09 -0400

    still not done, playing git

--- disemvowel_trolls.py
+++ disemvowel_trolls.py
@@ -9,14 +9,14 @@ input = str("This website is for losers LOL!")
 # No offense but,\nYour writing is among the worst I've ever read
 # "What are you, a communist?
 #        0    1    2    3    4
-vowels = ("a", "e", "i", "o", "u", "A", "E", "I", "O", "U")
+vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 
-def disemvowel string_:
-for t in input:
+def disemvowel (string_):
+    for t in range(len(input)):
         if t != vowels(0, 9):
-            print(str(string_))
+            print(string_)
         else: pass
     
-return string_
+    return string_
 
 
