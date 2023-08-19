18d5f8812f2a71e44b2a9573ad9227f7704b43de
TobyM <tobymarkham@gmail.com>
2023-08-18 22:01:52 -0400

    Whiteboard phase_using_for_loop

--- disemvowel_trolls.py
+++ disemvowel_trolls.py
@@ -1,7 +1,16 @@
-#Remove all vowels from the comment section [a, e, i, o, u]
+#Remove all vowels from the comment section [a, e, i, o, u] the "y" is not necessary in this exercise.
 #function take a string, and returns a sting with the vowels removed
+# Loop for loop.  
 
+# These are the test sentences:
+
+# "This website is for losers LOL!"
+# No offense but,\nYour writing is among the worst I've ever read
+# "What are you, a communist?
+#        0    1    2    3    4
+vowels=["a", "e", "i", "o", "u"]
 
 def disemvowel(string_):
-    
+    for t in vowels:
+
     return string_
