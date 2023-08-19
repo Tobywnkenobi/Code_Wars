1c250ce2b937e6c597d73c5da6e4a45e36d1a74a
TobyM <tobymarkham@gmail.com>
2023-08-18 23:12:29 -0400

    still working

--- disemvowel_trolls.py
+++ disemvowel_trolls.py
@@ -1,16 +1,20 @@
-#Remove all vowels from the comment section [a, e, i, o, u] the "y" is not necessary in this exercise.
-#function take a string, and returns a sting with the vowels removed
-# Loop for loop.  
+# Remove all vowels from the comment section [a, e, i, o, u] the "y" is not necessary in this exercise.
+# function take a string, and returns a sting with the vowels removed
+# Loop for loop.
 
 # These are the test sentences:
 
-# "This website is for losers LOL!"
+input = "This website is for losers LOL!"
+
 # No offense but,\nYour writing is among the worst I've ever read
 # "What are you, a communist?
 #        0    1    2    3    4
-vowels=["a", "e", "i", "o", "u"]
+vowels = ("a", "e", "i", "o", "u", "A", "E", "I", "O", "U")
 
 def disemvowel(string_):
-    for t in vowels:
-
-    return string_
+    for t in input:
+        if t != vowels():
+            print(string_)
+        else: pass
+            
+    # return string_
