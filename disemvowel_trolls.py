7a9f14553edeeec409241e0d76b22909104a8a3d
TobyM <tobymarkham@gmail.com>
2023-08-18 21:18:26 -0400

    mulitply_CW_C_troll start

---
+++ disemvowel_trolls.py
@@ -0,0 +1,7 @@
+#Remove all vowels from the comment section [a, e, i, o, u]
+#function take a string, and returns a sting with the vowels removed
+
+
+def disemvowel(string_):
+    
+    return string_
